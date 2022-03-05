98a2a4a0db6e21dfac8a1e29e8cb4e3f2d30daaf
任意のユーザ名 <exsrx97@gmail.com>
2022-03-06 00:23:22 +0900

    ✨ データの取得先をローカルへ変更

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,32 @@
 import Head from "next/head";
-import { useFetchKaruta } from "src/hooks/useFetchKaruta";
+import karutaList from "src/utils/karutaList.json";
 
 export default function Home() {
-
-  const {karuta,isLoading} = useFetchKaruta();
-
-  console.log(karuta);
+  const karuta = karutaList.karuta;
 
   return (
     <div>
       <Head>
         <title>Home</title>
       </Head>
-      <div className="text-red-600">Hello</div>
+      <div>
+        {karuta
+          ? karuta.map((item, index) => {
+              return (
+                <div key={index}>
+                  <p>{"{"} </p>
+                  <p>id:{item.no},</p>
+                  <p>kami:"{item.kami}",</p>
+                  <p>simo:"{item.simo}",</p>
+                  <p>kami_kana:"{item.kami_kana}",</p>
+                  <p>simo_kana:"{item.simo_kana}",</p>
+                  <p>simo_kana:"{item.sakusya}",</p>
+                  <p>{"}"},</p>
+                </div>
+              );
+            })
+          : null}
+      </div>
     </div>
   );
 }
