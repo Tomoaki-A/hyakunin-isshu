a5f5194e09e8831608d6ac336938ecdcff0354c1
任意のユーザ名 <exsrx97@gmail.com>
2022-03-07 02:13:13 +0900

    ✨ 百人一首データに現代語訳追加

---
+++ src/pages/test.jsx
@@ -0,0 +1,44 @@
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import { Modal } from "src/components/Modal";
+
+export default function Home() {
+
+  const [karuta, setKaruta] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("http://api.aoikujira.com/hyakunin/get2.php?fmt=json");
+      const data = await res.json();
+      setKaruta(data);
+    }
+    fetchData();
+  },[])
+
+  console.log(karuta);
+
+  return (
+    <div>
+      <Head>
+        <title>テスト</title>
+      </Head>
+      <div>
+      {karuta ? karuta.map((karuta, index) => {
+        return (
+          <div key={index}>
+            <div>{"{"}</div>
+            <div>"id":{karuta.no},</div>
+            <div>"kami":"{karuta.kami}",</div>
+            <div>"simo":"{karuta.simo}",</div>
+            <div>"kami_kana":"{karuta.kami_kana}",</div>
+            <div>"simo_kana":"{karuta.simo_kana}",</div>
+            <div>"sakusya":"{karuta.sakusya}",</div>
+            <div>"image":"{karuta.image}"</div>
+            <div>{"},"}</div>
+          </div>
+        )
+      }) : null}
+      </div>
+    </div>
+  );
+}
